182dbe01af593281c8229ebcb4446bbc53ef1e6a
Karol <karol.zakrzewski@atarise.com>
2023-02-02 11:01:03 +0100

    feat: setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,51 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  ignorePatterns: [".eslintrc.js", ".next"],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.js', '.next'],
   env: {
     browser: true,
     node: true,
     es6: true,
   },
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: false,
     },
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["functional", "deprecation"],
+  plugins: ['functional', 'deprecation'],
   extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   rules: {
-    "no-shadow": "off",
-    "import/no-deprecated": "warn",
-    "deprecation/deprecation": "warn",
-    "import/no-duplicates": "error",
-    "import/no-unresolved": "error",
-    "functional/no-let": "error",
-    "no-nested-ternary": "error",
-    complexity: ["error", 6],
-    curly: "error",
+    'no-shadow': 'off',
+    'import/no-deprecated': 'warn',
+    'deprecation/deprecation': 'warn',
+    'import/no-duplicates': 'error',
+    'import/no-unresolved': 'error',
+    'functional/no-let': 'error',
+    'no-nested-ternary': 'error',
+    complexity: ['error', 6],
+    curly: 'error',
   },
   settings: {
     react: {
-      version: "18.0.0",
+      version: '18.0.0',
     },
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project: __dirname,
       },
     },
   },
-};
+}
